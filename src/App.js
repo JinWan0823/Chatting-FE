f3fa111727e907a09bc2a3c8ec58208c1f691d45
jinWan0823 <dhksl823@gmail.com>
2023-11-06 17:39:25 +0900

    ✨ 간단 로그인 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import socket from "./server";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    askUserName();
+  }, []);
+
+  const askUserName = () => {
+    const userName = prompt("당신의 이름은?");
+    console.log(userName);
+
+    socket.emit("login", userName, (res) => {
+      if (res?.ok) {
+        setUser(res.data);
+      }
+    });
+  };
   return (
     <div>
       <div className="App"></div>
